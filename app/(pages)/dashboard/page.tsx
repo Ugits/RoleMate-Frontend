91399436fe1bd2ce6296a77245a4b56a8ad8bc95
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-21 03:42:52 +0100

    Add Delete me logic in button functionality and render button on dashboard

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/app/_global-components/Button";
 
 const page = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <div>Dashboard</div>
+      <Button title="Delete Me" color="crimson" pushPath="/home"/>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- app/_global-components/Button.tsx
+++ app/_global-components/Button.tsx
@@ -1,21 +1,76 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { IUsername } from "../_types/IUsername";
 
 interface ButtonProps {
   title: string;
-  pushPath?: string;
   color: string;
+  pushPath?: string;
+  username?: string;
 }
 
-export const Button = ({ title, pushPath, color }: ButtonProps) => {
+export const Button = ({ title, pushPath, color, username }: ButtonProps) => {
+  const [error, setError] = useState("");
   const router = useRouter();
+  const token = sessionStorage.getItem("accessToken");
 
   const handleClick = () => {
     if (title === "Logout") {
       sessionStorage.clear();
       const event = new Event("authChange");
       window.dispatchEvent(event);
+    }
+
+    if (title === "Delete Me") {
+      // logged in?
+      if (!token) {
+        window.alert("Not Authenticated. Please Login");
+        return;
+      }
+      //are u sure
+      const confirmed = window.confirm(
+        "Are you sure you want to delete this account?"
+      );
+
+      if (!confirmed) {
+        return;
+      }
+
+      //fetch
+      fetch(`http://localhost:8080/user/delete-me`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ username }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            return response.json().then((errData) => {
+              alert(errData.message);
+              throw new Error(errData.message);
+            });
+          }
+          return response.json();
+        })
+        .then((data: IUsername) => {
+          //alert user
+          alert(`User ${data.username} was successfully deleted`);
+
+          //clear storage
+          sessionStorage.clear();
+
+          //event signal
+          const event = new Event("authChange");
+          window.dispatchEvent(event);
+        })
+        .catch((error) => {
+          throw new Error("An error occurred while deleting the user.", error);
+        });
+      return;
     }
 
     if (!pushPath) return;
