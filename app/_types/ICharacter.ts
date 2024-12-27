1f512f8301e2bd4360f9196b60e9d12f61071868
Jonas Ullvahn <ullvahn@gmail.com>
2024-12-27 15:00:00 +0100

    Update Character interface

--- app/_types/ICharacter.ts
+++ app/_types/ICharacter.ts
@@ -1,6 +1,12 @@
 
-export interface ICharacter{
-    id: number
-    name: string
-    level: number
-}
+export interface ICharacter {
+    id: number;
+    name: string;
+    level: number;
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+  }
