c98721c9b43b1f03cc7eb5b61b353f96fea9ddce
Jonas Ullvahn <ullvahn@gmail.com>
2025-01-02 20:18:41 +0100

    Update home page css

--- app/page.tsx
+++ app/page.tsx
@@ -16,29 +16,34 @@ const page = () => {
 
   return (
     <main className="flex flex-col items-center justify-center pt-20 ">
-      <div className="text-center text-shadow-lg">
-        <h1 className="text-4xl font-bold mb-4">Welcome to RoleMate</h1>
-        <p className="text-base text-gray-700 mb-4">
-          Please log in to access your account details.
-        </p>
-      </div>
+      <div className="bg-black rounded-md p-4 bg-opacity-50">
+        <div className="text-center text-shadow-lg">
+          <h1 className="text-4xl font-bold mb-4">Welcome to RoleMate</h1>
+          <p className="text-base mb-4">
+            Please log in to access your account details.
+          </p>
+        </div>
 
-      <div className="flex flex-col justify-center items-center">
-        <p className="text-slate-400">No Account...?</p>
-        {/* Create New Account Button */}             {/*TODO CHANGE BUTTON TO CUSTOM BUTTON.TSX*/}
-        <div onClick={handleToggleSignupForm}>
-          {showRegisterForm ? (
-            <Button title="Hide Register Form" color="darkolivegreen" />
-          ) : (
-            <Button title="Create New Account" color="darkolivegreen" />
-          )}
+        <div className="flex flex-col justify-center items-center">
+          <p className="">No Account...?</p>
+          {/* Create New Account Button */}
+          <div onClick={handleToggleSignupForm}>
+            {showRegisterForm ? (
+              <Button title="Hide Register Form" color="darkolivegreen" />
+            ) : (
+              <Button title="Create New Account" color="darkolivegreen" />
+            )}
+          </div>
         </div>
       </div>
       <div>
         {/* Signup Form */}
         {showRegisterForm && (
           <div className="mt-6 w-full max-w-md">
-            <RegisterUser onRegisterUserSuccess={handleSignupSuccess} role="user"/>
+            <RegisterUser
+              onRegisterUserSuccess={handleSignupSuccess}
+              role="user"
+            />
           </div>
         )}
       </div>
